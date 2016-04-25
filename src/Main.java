116e3bb70b4a1b891cfb319e7a27795bd122375a
xidianfushuai <18792514506@163.com>
2016-04-25 12:12:37 +0800

    success

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class Main {
+	public static void main(String[] args) {
+		String password = "123456";
+		try {
+			MessageDigest instance = MessageDigest.getInstance("MD5");//获取MD5算法对象
+			byte[] digest = instance.digest(password.getBytes());//对字符串加密   返回字节数组
+			StringBuffer sb = new StringBuffer();
+			for (byte b : digest) {
+				int i = b & 0xff;
+				String hexString = Integer.toHexString(i);
+				//如果是一位   补0
+				if(hexString.length() < 2) {
+					hexString = "0" + hexString;
+				}
+				sb.append(hexString);
+				//System.out.println(hexString);
+			}
+			System.out.println(sb);
+		} catch (NoSuchAlgorithmException e) {
+			//没有该算法时 抛出该异常
+			e.printStackTrace();
+		}
+	}
+}
